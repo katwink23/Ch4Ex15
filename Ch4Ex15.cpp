a4c9764cbe944ed862eb89cfdab83dca77577446
Katlyn W. <krw8ateez@gmail.com>
2024-10-26 12:31:27 -0400

    Add project files.

---
+++ Ch4Ex15.cpp
@@ -0,0 +1,29 @@
+// File Name: Ch4Ex15.cpp
+// Programmer: Katlyn Winkelhake
+// Date: 10/26/2024
+// Requirements: Write a program that asks for the weight and distance shipped and display the charges.
+
+#include <iostream>
+using namespace std;
+
+void ShippingWeight();
+
+int main()
+{
+
+
+
+
+}
+
+void ShippingWeight() {
+
+	double packWeight;
+
+	cout << "What is the weight of the package? (in kg)" << endl;
+	cin >> packWeight;
+
+
+
+
+}
